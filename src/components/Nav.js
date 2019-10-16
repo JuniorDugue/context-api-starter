0204909412327d650826f0f908a182d9da8ccbac
Junior Dugue <jrdugue@gmail.com>
2019-10-15 20:34:23 -0400

    created nav component with few navs
    created nav component with few navs

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Nav(){
+  return(
+    <nav className="nav">
+      <a href="/">Home</a>
+      <a href="/">About</a>
+      <a href="/">Contact</a>
+    </nav>
+  )
+}
+
